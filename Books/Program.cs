b6cefeceb6aed87dde4b54e4068754f75fbb5ec5
Alexey Sokol <sokol.a.v.30@gmail.com>
2022-10-22 00:31:53 +0300

    [10/22]: First steps in programming

---
+++ Books/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+
+namespace Books
+{
+    internal class Program
+    {
+        //Подлючение без отображения прямой строки подключения
+        private static string connectionSting = ConfigurationManager.ConnectionStrings["BooksDB"].ConnectionString;
+        
+        private static SqlConnection sqlConnection = null;
+
+
+        static void Main(string[] args)
+        {
+            sqlConnection = new SqlConnection(connectionSting);
+            //открытие поключения
+            sqlConnection.Open();
+
+            Console.WriteLine("BooksApp");
+
+            SqlDataReader sqlDataReader = null;
+
+            string command = "";
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write(">> ");
+                command = UserCommand.SelectQuery();
+
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                
+                switch(command.Split(' ')[0].ToLower())
+                {
+                    case "select":
+
+                        sqlDataReader = sqlCommand.ExecuteReader();
+                        while (sqlDataReader.Read())
+                        {
+                            Console.WriteLine($"{sqlDataReader["Id"]} {sqlDataReader["TitleBook"]}" +
+                                $" {sqlDataReader["AuthorBook"]} {sqlDataReader["DateOfPublication"]}" +
+                                $" {sqlDataReader["BookQuantity"]} {sqlDataReader["Price"]}");
+                            Console.WriteLine(new String('-', 50));
+                        }
+
+                        break;
+                    case "insert":
+                        
+
+                        Console.WriteLine($"A line {sqlCommand.ExecuteNonQuery()} has been added.");
+
+                        break;
+                    case "update":
+
+
+                        break;
+                    case "delete":
+
+
+                        break;
+                    case "exit":
+                        if (sqlConnection.State == ConnectionState.Open) sqlConnection.Close();
+                        if (sqlDataReader != null) sqlDataReader.Close();
+                        flag = false;
+                        break;
+
+                    default:
+
+                        Console.WriteLine($"The entered command {command} is not correct");
+
+                        break;
+
+                }
+                
+
+
+            }
+            Console.WriteLine("Press any key..");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Books/UserCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books
+{
+    public static class UserCommand
+    {
+       
+        public static string SelectQuery()
+        {
+            
+            while (true)
+            {
+                Console.WriteLine("(Enter the command: select / insert / update / delete) or exit  ");
+                string command = Console.ReadLine().ToLower();
+                if (command.Equals("exit")) return "exit";
+                else if (command.Equals("select")) return "select * from [books]";
+                else if (command.Equals("insert"))
+                {
+                    Console.WriteLine("Enter title book: ");
+                    string titleBook = Console.ReadLine();
+
+                    Console.WriteLine("Enter author book: ");
+                    string authorBook = Console.ReadLine();
+
+                    Console.WriteLine("Enter date of publication: ");
+                    string dateOfPublication = Console.ReadLine();
+
+                    bool isValidValueInt = false;
+                    int bookQuantity = 0;
+                    while (isValidValueInt == false)
+                    {
+                        Console.WriteLine("Enter book quantity: ");
+                        if (int.TryParse(Console.ReadLine(), out bookQuantity)) isValidValueInt = true;
+                        else isValidValueInt = false;
+                    }
+
+                    decimal valueFloat = 0;
+                    bool isValidValueFloat = false;
+
+                    while (isValidValueFloat == false)
+                    {
+                        Console.WriteLine("Enter book price: ");
+                        if (decimal.TryParse(Console.ReadLine().Replace(',', '.'), out valueFloat)) isValidValueFloat = true;
+                        else isValidValueFloat = false;
+                    }
+                    return $"insert into books (TitleBook, AuthorBook,DateOfPublication, BookQuantity,Price) values ('{titleBook}', '{authorBook}', '{dateOfPublication}', '{bookQuantity}', '{valueFloat}')";
+
+                }
+                
+            }
+            
+        }
+    }
+}
